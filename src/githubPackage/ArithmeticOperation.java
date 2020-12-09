03b5566346bd98db2a86ffc6753ac23adbc29d66
username <ganieviko@gmail.com>
2020-12-08 23:27:05 -0500

    Arithmetic math

---
+++ src/githubPackage/ArithmeticOperation.java
@@ -0,0 +1,13 @@
+package githubPackage;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+
+    int a = 10;
+    int b = 4;
+    if (a > 4){
+        b = a + 10;
+    }
+        System.out.println(b);
+    }
+}
